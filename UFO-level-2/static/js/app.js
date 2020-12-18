c8b959b470e2effbf60ccb7427cccbd56d5087d8
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2020-12-18 16:18:16 -0500

    second draft works

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -86,47 +86,201 @@ function runEnter() {
 /// #######################################
 // ##########################################
 // Goal C: filter data by state.
-
+var state = data;
 // Select the button
-var button = d3.select("#filter-btn");
+var button = d3.select("#filter-btn1");
 
 // Create event handlers 
 // Define action when button is clicked
-button.on("click", runEnter);
+button.on("click", runState);
 
 // Select the form
 var form = d3.select("#form");
-form.on("submit",runEnter);
+form.on("submit", runState);
 
-function runEnter() {
+function runState() {
 
   // Prevent the page from refreshing
   d3.event.preventDefault();
   // clears table
   tbody.selectAll('*').remove();
   // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#state");
+  var inputElementS = d3.select("#state");
 
   // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+  var inputValueS = inputElementS.property("value");
 
-  console.log(inputValue);
-  //console.log(date);
+  console.log(inputValueS);
+  console.log(state);
 
-  var filteredData = date.filter(day => day.datetime === inputValue);
-  //var filteredData = state.filter(sta => sta.state === inputValue);
+  
+  var filteredState = state.filter(sta => sta.state === inputValueS);
 
-  console.log(filteredData);
+  console.log(filteredState);
 
   // // Step 4: Use d3 to append 1 cell per weather report value (Date, City, State, Country, Shape, Duration, Comments)
-  filteredData.forEach(function(et) {
+  filteredState.forEach(function(et) {
     console.log(et);
     var row = tbody.append("tr");
 
       Object.entries(et).forEach(function([key, value]) {
         console.log(key, value);
         //Append a cell to the row for each value
-        //in the weather report object
+        
+        var cell = row.append("td");
+        cell.text(value)
+      })
+  })
+};
+
+
+/// #######################################
+/// #######################################
+// ##########################################
+// Goal C: filter data by Country.
+var city = data;
+// Select the button
+var button = d3.select("#filter-btn2");
+
+// Create event handlers 
+// Define action when button is clicked
+button.on("click", runCity);
+
+// Select the form
+var form = d3.select("#form");
+form.on("submit", runCity);
+
+function runCity() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // clears table
+  tbody.selectAll('*').remove();
+  // Select the input element and get the raw HTML node
+  var inputElementCity = d3.select("#city");
+
+  // Get the value property of the input element
+  var inputValueCity = inputElementCity.property("value");
+
+  console.log(inputValueCity);
+  //console.log(state);
+
+  
+  var filteredCity = city.filter(ci => ci.city === inputValueCity);
+
+  console.log(filteredCity);
+
+  // // Step 4: Use d3 to append 1 cell per weather report value (Date, City, State, Country, Shape, Duration, Comments)
+  filteredCity.forEach(function(et) {
+    console.log(et);
+    var row = tbody.append("tr");
+
+      Object.entries(et).forEach(function([key, value]) {
+        console.log(key, value);
+        //Append a cell to the row for each value
+        
+        var cell = row.append("td");
+        cell.text(value)
+      })
+  })
+};
+
+/// #######################################
+/// #######################################
+// ##########################################
+// Goal D: filter data by Country.
+var country = data;
+// Select the button
+var button = d3.select("#filter-btn3");
+
+// Create event handlers 
+// Define action when button is clicked
+button.on("click", runCountry);
+
+// Select the form
+var form = d3.select("#form");
+form.on("submit", runCountry);
+
+function runCountry() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // clears table
+  tbody.selectAll('*').remove();
+  // Select the input element and get the raw HTML node
+  var inputElementCO = d3.select("#country");
+
+  // Get the value property of the input element
+  var inputValueCO = inputElementCO.property("value");
+
+  console.log(inputValueCO);
+  //console.log(state);
+
+  
+  var filteredCountry = country.filter(co => co.country === inputValueCO);
+
+  console.log(filteredCountry);
+
+  // // Step 4: Use d3 to append 1 cell per weather report value (Date, City, State, Country, Shape, Duration, Comments)
+  filteredCountry.forEach(function(et) {
+    console.log(et);
+    var row = tbody.append("tr");
+
+      Object.entries(et).forEach(function([key, value]) {
+        console.log(key, value);
+        //Append a cell to the row for each value
+        
+        var cell = row.append("td");
+        cell.text(value)
+      })
+  })
+};
+
+/// #######################################
+/// #######################################
+// ##########################################
+// Goal F: filter data by Shape.
+var shape = data;
+// Select the button
+var button = d3.select("#filter-btn4");
+
+// Create event handlers 
+// Define action when button is clicked
+button.on("click", runShape);
+
+// Select the form
+var form = d3.select("#form");
+form.on("submit", runShape);
+
+function runShape() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  // clears table
+  tbody.selectAll('*').remove();
+  // Select the input element and get the raw HTML node
+  var inputElementShape = d3.select("#shape");
+
+  // Get the value property of the input element
+  var inputValueShape = inputElementShape.property("value");
+
+  console.log(inputValueShape);
+  //console.log(state);
+
+  
+  var filteredShape = shape.filter(sha => sha.shape === inputValueShape);
+
+  console.log(filteredShape);
+
+  // // Step 4: Use d3 to append 1 cell per weather report value (Date, City, State, Country, Shape, Duration, Comments)
+  filteredShape.forEach(function(et) {
+    console.log(et);
+    var row = tbody.append("tr");
+
+      Object.entries(et).forEach(function([key, value]) {
+        console.log(key, value);
+        //Append a cell to the row for each value
+        
         var cell = row.append("td");
         cell.text(value)
       })
